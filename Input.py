1bfc87992dbaab2ef8abb27cc2dc276f78533dd1
KokoszkaJakub <kokoszka.j@gmail.com>
2020-09-14 22:43:43 +0200

    pop1

--- Input.py
+++ Input.py
@@ -1,12 +1,25 @@
-Name = input("hello, give me youre name: ")
+Name = input("hello, give me your name: ")
 
 print("hello " + Name)
 
 age = int(input("how old are you ? "))
-print("you are " + str(age) + " years old")
 
-if age >= 18:
+
+def printHello():
+    print("you are " + str(age) + " years old")
+
+
+if 99 > age >= 18:
+    printHello()
     print("jesteś pelnoletni")
+
+elif age > 100:
+    print("you are dead")
+
+elif age < 0:
+    print("error ")
+
 
 else:
     print("jestes jeszcze dzickiem")
+    printHello()
