5fea4845428e28078ed7eec56af3890180976771
KokoszkaJakub <kokoszka.j@gmail.com>
2020-09-15 22:02:42 +0200

    pop2

--- Input.py
+++ Input.py
@@ -1,9 +1,25 @@
 Name = input("hello, give me your name: ")
-
-print("hello " + Name)
+SName = input("and your surname ? ")
+print("hello " + Name + " " + SName)
+mail = input("we need more info, what your email adress? ")
 
 age = int(input("how old are you ? "))
 
+listOfNames = []
+listOfSurname = []
+listofMail = []
+listofUsers = []
+
+user1 = "user: " + Name + " " + SName + ", email: " + mail
+
+listOfNames.append(Name)
+listOfSurname.append(SName)
+listofMail.append(mail)
+listofUsers.append(user1)
+
+print(listOfNames + listOfSurname + listofMail)
+print()
+print(listofUsers)
 
 def printHello():
     print("you are " + str(age) + " years old")
