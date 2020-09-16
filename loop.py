ba15bab93a94d2f3856f677f9355bac8dfd36318
KokoszkaJakub <kokoszka.j@gmail.com>
2020-09-16 22:57:11 +0200

    pop2

---
+++ loop.py
@@ -0,0 +1,14 @@
+i = 1
+
+while i <= 10:
+    print(i)
+    i = i +1
+print("--------------")
+i = 1
+for letter in "Loop with if":
+    if letter == "H":
+        print("this is first letter")
+    else:
+        print(i)
+        i = i +1
+    print(letter)
