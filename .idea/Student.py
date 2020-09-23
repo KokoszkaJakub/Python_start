80b3488c9656176e3f20abcbb268a71a8059a232
KokoszkaJakub <kokoszka.j@gmail.com>
2020-09-23 22:33:01 +0200

    class

---
+++ .idea/Student.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name, sName, age, yearOfStud, is_scholarship):
+        self.name = name
+        self.sName = sName
+        self.age = age
+        self.yearOfStud = yearOfStud
+        self.is_scholarship = is_scholarship
+
+
---
+++ .idea/studentapk.py
@@ -0,0 +1,12 @@
+from Student import Student
+
+student1 = Student("Bob", "Kovalsky", 24, 2, False)
+student2 = Student("Kriss", "Smith", 26, 4, True)
+
+print(student1.name)
+print(student1.sName)
+print(student1.age)
+print(student1.yearOfStud)
+print(student1.is_scholarship)
+
+
