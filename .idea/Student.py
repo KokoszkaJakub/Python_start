4ab4301f3b4c24a30e88ccc588e0499d921bfc33
KokoszkaJakub <kokoszka.j@gmail.com>
2020-09-27 20:51:48 +0200

    class

--- .idea/Student.py
+++ .idea/Student.py
@@ -1,9 +1,9 @@
 class Student:
-    def __init__(self, name, sName, age, yearOfStud, is_scholarship):
+    def __init__(self, name, sName, age, yearOfStud):
         self.name = name
         self.sName = sName
         self.age = age
         self.yearOfStud = yearOfStud
-        self.is_scholarship = is_scholarship
+
 
 
--- .idea/studentapk.py
+++ .idea/studentapk.py
@@ -1,12 +1,18 @@
 from Student import Student
 
-student1 = Student("Bob", "Kovalsky", 24, 2, False)
-student2 = Student("Kriss", "Smith", 26, 4, True)
+student1 = Student("Bob", "Kovalsky", 24, 2)
+student2 = Student("Kriss", "Smith", 26, 4)
+student3 = Student("Stev", "asd", "24", "1")
+student4 = Student
 
-print(student1.name)
-print(student1.sName)
-print(student1.age)
-print(student1.yearOfStud)
-print(student1.is_scholarship)
+student4.name = input("Hello, whats your name ? ")
+student4.sName = input("and your sername ? ")
+student4.age = input("how old are you ? ")
+student4.yearOfStud = input("Yours year of study are ? ")
+
+
+
+
+print("Hello " + student4.name + " " + student4.sName + " and welcome in ouers school !")
 
 
